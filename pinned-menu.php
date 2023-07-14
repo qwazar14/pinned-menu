5104a3d48c13609b15345d2c526bb39d3a161899
qwazar14 <zenez89@gmail.com>
2023-07-14 14:55:58 +0300

    feat. 1.3. Removed Live Search and Search button

--- pinned-menu.php
+++ pinned-menu.php
@@ -3,7 +3,7 @@
 Plugin Name: Pinned Menu
 Description: Adds a pinned menu to the bottom of all pages.
 Author: Maksym Mezhyrytskyi
-Version: 1.2
+Version: 1.3
 Plugin URI: https://github.com/qwazar14/pinned-menu/
 Author URI: https://github.com/qwazar14/
 */
@@ -18,10 +18,7 @@ class PinnedMenu
 
     function enqueue_scripts()
     {
-        wp_enqueue_script('jquery');
-        wp_enqueue_script('jquery-ui-autocomplete');
         wp_enqueue_style('pinned-menu-style', plugins_url('pinned-menu.css', __FILE__));
-        wp_enqueue_script('mpinned-menu-script', plugins_url('pinned-menu.js', __FILE__), array('jquery'), '1.0.0', true);
     }
 
     function display_menu()
@@ -43,16 +40,9 @@ class PinnedMenu
         $menu = '<div class="mf-navigation-mobile" id="mf-navigation-mobile">
         <div class="navigation-list">
             <a href="' . get_home_url() . '" class="navigation-icon navigation-mobile_home"><i class="icon-home"></i> Головна</a>
-            <div class="navigation-icon navigation-mobile_cat">
+            <div class="navigation-icon pinned-menu-navigation-mobile_cat">
                 <i class="icon-menu"></i> Категорії
                 <ul class="navigation-mobile_subcat">' . $category_list . '</ul>
-            </div>
-            <a href="#" class="navigation-icon navigation-mobile_search" id="show_search">
-                <i class="icon-magnifier"></i> Пошук
-            </a>
-            <div id="live_searchform" style="display: none;">' .
-            do_shortcode('[fibosearch]') .
-            '<div id="live_search_results"></div>
             </div>
             <a href="' . wc_get_cart_url() . '" class="navigation-icon navigation-mobile_cart cart-contents "><i class="icon-bag2"></i><span class="mini-item-counter mf-background-primary">' . WC()->cart->get_cart_contents_count() . '</span> Кошик</a>
             <a href="' . get_permalink(get_option('woocommerce_myaccount_page_id')) . '" class="navigation-icon navigation-mobile_account"><i class="icon-user"></i> Акаунт</a>
